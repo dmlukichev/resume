974b0e94ad428477e313dee64e56f6e0c11f7752
Dmitry Lukichev <dmlukichev@gmail.com>
2020-02-07 14:39:10 +0300

    feat: populated with data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import * as faker from 'faker';
+import {
+  faPhoneSquare as phoneIcon,
+  faEnvelope as emailIcon,
+  faMapMarkedAlt as addressIcon,
+  faCalendar as calendarIcon,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub as githubIcon,
+  faLinkedin as linkedinIcon,
+  faStackOverflow as stackOverflowIcon
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'resume';
+  icons = {
+    phoneIcon, emailIcon, addressIcon, calendarIcon,
+    githubIcon, linkedinIcon, stackOverflowIcon
+  };
+
+  skills = [
+    { name: 'Javascript', value: 10 },
+    { name: 'TypeScript', value: 9 },
+    { name: 'PHP', value: 7 },
+    { name: 'Python', value: 6 },
+    { name: 'SQL', value: 8 },
+    { name: 'Angular', value: 10 },
+    { name: 'NodeJS', value: 9 },
+    { name: 'React', value: 9 },
+    { name: 'HAPI', value: 6 },
+    { name: 'NestJS', value: 8 },
+  ];
+
+  languages = [
+    { name: 'English', value: 8 },
+    { name: 'Russian', value: 10 },
+  ];
 }
